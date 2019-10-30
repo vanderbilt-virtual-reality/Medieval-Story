f7f78b424823eb008c2b1712de8f9be478ea7b8b
Chris Alexander <christopher.j.alexander@vanderbilt.edu>
2019-10-30 18:30:45 -0500

    Added a bunch of new assets

---
+++ Assets/European_Megapack/_Scripts/AnimHelper.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimHelper : MonoBehaviour {
+
+    public List<Animator> modelAnimators;
+    public Text animName;
+    public int currentClipNumber;
+
+    private AnimationClip[] _animationClips;
+
+    void Start () {
+
+        // Get a list of the animation clips
+        _animationClips = modelAnimators[0].runtimeAnimatorController.animationClips;
+
+        // Iterate over the clips and gather their information
+        foreach (AnimationClip animClip in _animationClips)
+        {
+            Debug.Log(animClip.name + ": " + animClip.length);
+        }
+
+        currentClipNumber = 0;
+        PlayAnimationClip(currentClipNumber);
+    }
+
+	void Update () {
+		
+	}
+
+    public void OnNextAnimButtonPressed()
+    {
+        currentClipNumber++;
+        if(currentClipNumber > _animationClips.Length - 1)
+        {
+            currentClipNumber = 0;
+        }
+        PlayAnimationClip(currentClipNumber);
+    }
+
+    public void OnPrevAnimButtonPressed()
+    {
+        currentClipNumber--;
+        if (currentClipNumber < 0)
+        {
+            currentClipNumber = _animationClips.Length - 1;
+        }
+        PlayAnimationClip(currentClipNumber);
+    }
+
+    private void PlayAnimationClip(int __clipNumber)
+    {
+        foreach (Animator __modelAnimator in modelAnimators)
+        {
+            __modelAnimator.Play(_animationClips[__clipNumber].name);
+            animName.text = _animationClips[__clipNumber].name;
+        }
+       
+    }
+
+}
---
+++ Assets/GothicChurch/SampleArenaPlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace FullInspector.Samples.Games.Arena
+{
+    public class SampleArenaPlayerController : MonoBehaviour
+    {
+        //("The speed that the player moves at")
+        public float Speed = 1;
+
+        protected void Update()
+        {
+            //move forward/backward
+            transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * Speed * Time.deltaTime);
+            transform.Translate(Input.GetAxis("Horizontal") * Vector3.right * Speed * Time.deltaTime);
+        }
+    }
+}
